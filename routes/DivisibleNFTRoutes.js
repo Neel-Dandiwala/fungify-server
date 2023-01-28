06beed21e43656867390b13c661cec05d91b2140
Neel Dandiwala <neeldandi@gmail.com>
2023-01-29 03:55:54 +0530

    issues resolved - blockchain 3.0

--- routes/DivisibleNFTRoutes.js
+++ routes/DivisibleNFTRoutes.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const { _mint, _transferToken, _unitsOwnedOfAToken, _divisibilityOfAToken, _totalSupplyView } = require('../controller/DivisibleNFTController');
-const router = express_1.default.Router();
 router.get('/nft/totalSupply', _totalSupplyView);
 router.post('/nft/mint', _mint);
 router.post('/nft/transfer', _transferToken);
