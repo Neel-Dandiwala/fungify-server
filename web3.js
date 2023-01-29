6bbfd3d81f4e85cc9f6a2f49487474b71718b7de
Hasnain01-hub <hasnainsayyed833@gmail.com>
2023-01-29 13:22:20 +0530

    admin api fixes

--- web3.js
+++ web3.js
@@ -1,8 +1,8 @@
 // import Web3 from "web3";
 // import fs from "fs";
 // import { connection } from "./connection";
-const Web3 = require('web3');
-const fs = require('fs');
+const Web3 = require("web3");
+const fs = require("fs");
 // const connection = require('./connection');
 
 let _web3;
@@ -32,17 +32,21 @@ let _web3;
 // }
 
 exports.web3config = async (con) => {
-    const web3_ = await con.connection.db.collection('config').findOne({_id: 'web3'})
-    _web3 = web3_
-    console.log(_web3)
-}
+  const web3_ = await con.connection.db
+    .collection("config")
+    .findOne({ _id: "web3" });
+  _web3 = web3_;
+  console.log(_web3);
+};
 
 exports.web3 = () => {
-    return new Web3(
-    new Web3.providers.HttpProvider(_web3.web3_link));
-}
+  return new Web3(
+    new Web3.providers.HttpProvider("https://4781-42-106-240-177.ngrok.io")
+  );
+};
 
 // exports.web3 = async () => {
 
-
-exports.DivisibleNftsABI = JSON.parse(fs.readFileSync('blockchain/build/contracts/DivisibleNFTs.json', 'utf-8'));
+exports.DivisibleNftsABI = JSON.parse(
+  fs.readFileSync("blockchain/build/contracts/DivisibleNFTs.json", "utf-8")
+);
